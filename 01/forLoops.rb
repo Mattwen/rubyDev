78e704a02c897529df82fa9b1275bc03a8464d71
mattwen <mattwen@uw.edu>
2017-05-09 00:08:04 -0700

    init

---
+++ 01/forLoops.rb
@@ -0,0 +1,18 @@
+# Print the keys and values in array
+nums = [1,2,3,4,5]
+for elem in nums
+	puts elem
+end
+
+# Print the keys and values in a hash
+hash = {:a=>1, :b=>2, :c=>3}
+for key,value in hash
+	puts "#{key} => #{value}"
+end
+
+# while loop
+x = 10
+while x >= 0 do
+	puts x
+x = x - 1
+end
